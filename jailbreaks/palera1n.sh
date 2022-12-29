5171440a44369324a5d4b5cd13fb6723bada10d0
Jay <jayluxferro@sperixlabs.org>
2022-12-29 23:54:16 +0000

    Init 1

--- jailbreaks/palera1n.sh
+++ jailbreaks/palera1n.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-git clone --recursive https://github.com/palera1n/palera1n
+folder_name="palera1n"
+
+if [ -d "./$folder_name" ]; then
+  rm -rf $folder_name
+fi
+
+git clone --recursive https://github.com/palera1n/palera1n $folder_name
